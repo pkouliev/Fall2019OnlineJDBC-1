e67206ad290aa09c7f2e561e07da155926e94b1d
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-05-12 21:50:56 -0400

    update

--- src/test/java/com/jdbc/day1/InsertAndDeleteTest.java
+++ src/test/java/com/jdbc/day1/InsertAndDeleteTest.java
@@ -1,12 +1,25 @@
 package com.jdbc.day1;
 
+import java.net.URI;
+import java.sql.*;
+
 public class InsertAndDeleteTest {
 
-    String URL = "jdbc:oracle:thin:@3.90.175.72:1521:xe";
-    String username = "hr";
-    String password = "hr";
+    static String URL = "jdbc:oracle:thin:@3.90.175.72:1521:xe";
+    static String username = "hr";
+    static String password = "hr";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager.getConnection(URL, username, password);
+        Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 
+        String INSERT_QUERY = "INSERT INTO employees VALUES(223, 'Hasan', 'Mammadov', 'hasan@cybertek.com', '777-777-7777', SYSDATE, 'SDET', 14999, 0, NULL, NULL)";
+        String DELETE_QUERY = "DELETE FROM employees WHERE employee_id = 223";
+
+        ResultSet resultSet = statement.executeQuery(INSERT_QUERY);
+
+        resultSet.close();
+        statement.close();
+        connection.close();
     }
 }
